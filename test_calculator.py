ef22efef1acc1f04f93321ec98c791c9b4ac7096
Kavya Patel <patelkavya173@gmail.com>
2025-04-10 13:27:50 -0400

    link

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,6 @@
+#https://github.com/Adventure7606/Lab10-KP-XG.git
 import unittest
 from calculator import *
-#https://github.com/Adventure7606/Lab10-KP-XG.git
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
     # def test_add(self): # 3 assertions
