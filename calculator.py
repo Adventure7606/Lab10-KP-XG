cd2a490a21a5075a16b3a63dc69755bc33139026
xci219 <xcigriffiths@hotmail.com>
2025-04-10 12:15:47 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -4,8 +4,29 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
+class Calculator:
+    import math
+    def add(a, b):
+        return a + b
+
+    def sub(a, b):
+        return a - b
+
+    def mul(a, b):
+        return a * b
+
+    def div(a, b):
+        if a == 0:
+            raise ZeroDivisionError("Division by zero is undefined.")
+        return b / a
+
+    def log(a, b):
+        if a <= 0 or a == 1 or b <= 0:
+            raise ValueError("Logarithm undefined for these values.")
+        return math.log(b, a)
+
+    def exp(a, b):
+        return a ** b
+
 
 
